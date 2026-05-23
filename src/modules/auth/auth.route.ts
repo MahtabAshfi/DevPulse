8ab7f8f8dcc7ae6fb4fe2034fc5487f2dc286212
MahtabAshfi <mahtabashfi1625@gmail.com>
2026-05-23 14:21:33 +0600

    routing for authentication added

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
+import { registerUser, loginUser } from './auth.controller';
 
 const router = Router();
-
+router.post('/signup', registerUser);
+router.post('/login', loginUser);
 export default router;
