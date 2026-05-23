6691cf9f18e40c2a34083d2173514475e13f83d2
MahtabAshfi <mahtabashfi1625@gmail.com>
2026-05-23 15:17:29 +0600

    issue routes added

--- src/modules/issues/issues.route.ts
+++ src/modules/issues/issues.route.ts
@@ -1,5 +1,12 @@
 import { Router } from 'express';
+import { createIssueRecord, fetchAllIssues, fetchSingleIssue, modifyIssue, removeIssue } from './issues.controller';
+import { authenticateToken, requireRole } from '../../middleware/auth.middleware';
 
 const router = Router();
+router.post('/', authenticateToken, createIssueRecord);
+router.get('/', fetchAllIssues);
+router.get('/:id', fetchSingleIssue);
+router.patch('/:id', authenticateToken, modifyIssue);
+
 
 export default router;
